9ba5973bd79b24822438c7ee537cf2de7ac713d5
fashionfz <ibcm@qq.com>
2015-05-11 17:58:52 +0800

    c

---
+++ src/main/java/com/game/bean/Hero.java
@@ -0,0 +1,173 @@
+package com.game.bean;
+
+/**
+ * 英雄
+ * @author dell
+ *
+ */
+public class Hero {
+	
+	private int power;//力量
+	
+	private double powerPlus;//部队生命加成
+
+	private int quick;//敏捷
+	
+	private double quickPlus;//部队速度加成
+	
+	private int command;//智力
+	
+	private double commandPlus;//英雄技能伤害加成
+	
+	//========================
+	
+	private int attack;
+	
+	private double attackPlus;//部队攻击加成
+	
+	private int defense;
+	
+	private double defensePlus;//部队防御加成
+
+	/**
+	 * @return the power
+	 */
+	public int getPower() {
+		return power;
+	}
+
+	/**
+	 * @param power the power to set
+	 */
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	/**
+	 * @return the powerPlus
+	 */
+	public double getPowerPlus() {
+		return powerPlus;
+	}
+
+	/**
+	 * @param powerPlus the powerPlus to set
+	 */
+	public void setPowerPlus(double powerPlus) {
+		this.powerPlus = powerPlus;
+	}
+
+	/**
+	 * @return the quick
+	 */
+	public int getQuick() {
+		return quick;
+	}
+
+	/**
+	 * @param quick the quick to set
+	 */
+	public void setQuick(int quick) {
+		this.quick = quick;
+	}
+
+	/**
+	 * @return the quickPlus
+	 */
+	public double getQuickPlus() {
+		return quickPlus;
+	}
+
+	/**
+	 * @param quickPlus the quickPlus to set
+	 */
+	public void setQuickPlus(double quickPlus) {
+		this.quickPlus = quickPlus;
+	}
+
+	/**
+	 * @return the command
+	 */
+	public int getCommand() {
+		return command;
+	}
+
+	/**
+	 * @param command the command to set
+	 */
+	public void setCommand(int command) {
+		this.command = command;
+	}
+
+	/**
+	 * @return the commandPlus
+	 */
+	public double getCommandPlus() {
+		return commandPlus;
+	}
+
+	/**
+	 * @param commandPlus the commandPlus to set
+	 */
+	public void setCommandPlus(double commandPlus) {
+		this.commandPlus = commandPlus;
+	}
+
+	/**
+	 * @return the attack
+	 */
+	public int getAttack() {
+		return attack;
+	}
+
+	/**
+	 * @param attack the attack to set
+	 */
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	/**
+	 * @return the attackPlus
+	 */
+	public double getAttackPlus() {
+		return attackPlus;
+	}
+
+	/**
+	 * @param attackPlus the attackPlus to set
+	 */
+	public void setAttackPlus(double attackPlus) {
+		this.attackPlus = attackPlus;
+	}
+
+	/**
+	 * @return the defense
+	 */
+	public int getDefense() {
+		return defense;
+	}
+
+	/**
+	 * @param defense the defense to set
+	 */
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+	/**
+	 * @return the defensePlus
+	 */
+	public double getDefensePlus() {
+		return defensePlus;
+	}
+
+	/**
+	 * @param defensePlus the defensePlus to set
+	 */
+	public void setDefensePlus(double defensePlus) {
+		this.defensePlus = defensePlus;
+	}
+	
+	
+}
--- src/main/java/com/game/service/WarService.java
+++ src/main/java/com/game/service/WarService.java
@@ -3,6 +3,8 @@ package com.game.service;
 public class WarService {
 
 	public static void war(Faction f1,Faction f2){
+		int pos1 = 1;
+		int pos2 =1;
 
 		int time = 0;
 		int t1 = (int) (1000.00/f1.getSpeed()*1000.00);//毫秒
@@ -17,13 +19,24 @@ public class WarService {
 		while(true){
 			if(time/1000 >= 60)
 				break;
+			//=====================
+			if(pos1%6 == 0)
+				pos1 = 1;
+			if(pos2%6 == 0)
+				pos2 = 1;
+			//=====================
 			time+=temp;
-			if(time%t1 == 0 && time%t2 == 0){
+			//=====================
+			if(time%t1 == 0 && time%t2 == 0 && pos1 == pos2){
 				System.out.println("1,2对击");
+				pos1++;
+				pos2++;
 			}else if(time%t1 == 0){
 				System.out.println("1攻击");
+				pos1++;
 			}else if(time%t2 == 0){
 				System.out.println("2攻击");
+				pos2++;
 			}
 		}
 	}
@@ -50,5 +63,15 @@ public class WarService {
 		Faction f2 = new Faction();
 		f2.setSpeed(401);
 		WarService.war(f1, f2);
+	}
+	
+	/**
+	 * log运算
+	 * @param value
+	 * @param base
+	 * @return
+	 */
+	public static double log(int value,int base){
+		return Math.log(value)/Math.log(base);
 	}
 }
