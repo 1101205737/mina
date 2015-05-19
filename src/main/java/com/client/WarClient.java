63f2d668182aeb715ff878100f041a69af6ca24a
fashionfz <ibcm@qq.com>
2015-05-19 12:04:15 +0800

    c

--- src/main/java/com/client/WarClient.java
+++ src/main/java/com/client/WarClient.java
@@ -1,6 +1,9 @@
 package com.client;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+import com.mina.GameClient;
 
 public class WarClient {
 
@@ -9,5 +12,13 @@ public class WarClient {
 		String report = (String) map.get("report");
 		String[] items = report.split("#");
 		System.out.println(items.length);
+	}
+	
+	
+	public static void main(String[] args) throws IOException{
+		Map map = new HashMap();
+		map.put("class", "CardAction");
+		map.put("method", "extract");
+		GameClient.sendToServer(map,"com.client.WarClient","parse");
 	}
 }
--- src/main/java/com/mina/GameClient.java
+++ src/main/java/com/mina/GameClient.java
@@ -1,8 +1,6 @@
 package com.mina;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.mina.core.future.ConnectFuture;
@@ -11,15 +9,7 @@ import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 
-class GameClient {
-
-	public static void main(String[] args) throws IOException{
-		Map map = new HashMap();
-		map.put("class", "CardAction");
-		map.put("method", "extract");
-		sendToServer(map,"com.client.WarClient","parse");
-	}
-	
+public class GameClient {
 	
 	public static void sendToServer(Map param,String _class,String callback){
 		IoConnector connector = new NioSocketConnector();
